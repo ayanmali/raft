3a0e42a8a59d5e5dcd0293890932de9992d778ee
Ayan Ali <ayan_ali2004@hotmail.com>
2026-06-29 01:19:38 +0000

    fixing log

--- rpc/protocol/client.cpp
+++ rpc/protocol/client.cpp
@@ -3,6 +3,7 @@
 #include "../../errors.hpp"
 #include "payloads.hpp"
 #include <expected>
+#include <iostream>
 
 /* Inbound */
 
@@ -61,13 +62,13 @@ constexpr auto PARSER_TABLE = make_parser_table();
 
 std::expected<RpcMessage, const char*> parse_rbuf(ClientConn* c) {
     if (c->rbuf.size() < sizeof(uint32_t)) { return Unexpected("not enough data to read"); } // need to see message size first
-    #ifdef DEBUG
+
     std::cout << "rbuf bytes:\n";
     for (auto b : c->rbuf) {
-        std::cout << static_cast<int>(b)
+        std::cout << static_cast<int>(b);
     }
     std::cout << "\n";
-    #endif
+
     uint32_t message_size;
     std::memcpy(&message_size, c->rbuf.data(), sizeof(message_size));
     message_size = ntohl(message_size);
